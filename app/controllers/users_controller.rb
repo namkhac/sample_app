43963a85884457261c27b5e1c73152e54fb5e98e
namkhac <namkhac.email@gmail.com>
2019-04-01 10:47:12 +0700

    modelling-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = t(".welcome_sample_app")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def find_by_id
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      flash[:danger] = t(".controllers.invalid_user")
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-	root "application#hello"
-	root "users#index"
-	root "static_pages#home"
-	 get  "static_pages/home"
-	 get  "/help",    to: "static_pages#help", as: "helf"
-	 get  "/about",   to: "static_pages#about"
-	 get  "/contact", to: "static_pages#contact"
-		get  "/signup",  to: "users#new"
+  root "application#hello"
+  root "users#index"
+  root "static_pages#home"
+  get  "static_pages/home"
+  get  "/help",    to: "static_pages#help", as: "helf"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
+  get  "/signup",  to: "users#new"
+  resources :users
 end
